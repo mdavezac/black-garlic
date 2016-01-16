fec67b9a0df8b1548c01d65a8418a5cdb9492548
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-01-16 20:06:49 +0000

    Error when creating ctags call line

--- _states/ctags.py
+++ _states/ctags.py
@@ -5,13 +5,13 @@ def run(name, fields="+l", exclude=None, ctags="/usr/local/bin/ctags",
         fields = []
     elif isinstance(fields, str):
         fields = [fields]
-    fields = " --fields=".join(fields)
+    fields = " --fields=".join([""] + fields)
 
     if exclude is None:
         exclude = []
     elif isinstance(exclude, str):
         exclude = [exclude]
-    exclude = " --exclude=".join(exclude)
+    exclude = " --exclude=".join([""] + exclude)
 
     cmd = "{ctags} {fields} {exclude} {name}".format(
         ctags=ctags, fields=fields, exclude=exclude, name=name)
