49f626b7f48e5b9c2ab02f6d380894fbb4309447
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-01-17 21:04:24 +0000

    Replace salt.sh with pinch.sh

---
+++ bin/pinch.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# Makes it easier to call salt
+
+# will contain all sls
+args=()
+do_sync=false
+
+# parse options
+while [[ $# > 0 ]]; do
+  current="$1"
+  case $current in
+      -s|--sync)
+        do_sync=true
+      ;;
+      *)
+        args+=($current)
+      ;;
+  esac
+  shift
+done
+
+if [[ "${do_sync}" = "true" ]] ; then
+  salt-call --local saltutil.sync_all
+fi
+
+if [[ ${#args} -ge 0 ]]; then
+  for i in "${args[@]}"; do
+    salt-call --local state.sls $i
+  done
+else
+  salt-call --local state.highstate
+fi
--- bin/salt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-source salt-env/bin/activate
-if [[ "$(uname)" -eq "Darwin" ]] ; then
-  $(which salt-call) --local state.highstate $*
-else
-  sudo $(which salt-call) --local state.highstate $*
-fi
