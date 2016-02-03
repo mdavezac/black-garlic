02ab799f91bda7cc6d203f07acc0f8c6db8c436b
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-02-03 10:38:55 +0000

    condiment_dir grain did not work

--- _grains/user.py
+++ _grains/user.py
@@ -9,14 +9,9 @@ def _home():
   from os.path import expanduser
   return {'userhome': expanduser("~" + getuser())}
 
-def condiment_dir():
-    from os.path import dirname
-    return dirname(__file__)
-
 def main():
   grains = {}
   grains.update(_user())
   grains.update(_shell())
   grains.update(_home())
-  grains['condiment_dir'] = condiment_dir()
   return grains
